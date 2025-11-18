7eb949aa7896188f7ee2bbfcf4861e1703d35e2f
samanthakim <samanthakim3787@gmail.com>
2025-11-18 00:28:02 -0500

    parking violation object (common tier)

--- src/main/java/project/common/ParkingViolation.java
+++ src/main/java/project/common/ParkingViolation.java
@@ -1,5 +1,7 @@
 package project.common;
 
+import java.util.Objects;
+
 public class ParkingViolation {
 
     private final String timestamp;
@@ -21,6 +23,57 @@ public class ParkingViolation {
         this.zipCode = zipCode;
     }
 
+    public String getTimestamp() {
+        return timestamp;
+    }
 
+    public int getFine() {
+        return fine;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getVehicleId() {
+        return vehicleId;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public String getViolationId() {
+        return violationId;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    @Override
+    public String toString() {
+        return "Parking Violation: " + "timestamp = " + timestamp + '\'' + "fine = " + fine + '\'' +
+                "description = " + description + '\'' + "vehicleId = " + vehicleId + '\'' +
+                "state = " + state + '\'' + "violationId" + violationId + '\'' + "zipCode" + zipCode;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof ParkingViolation)) {
+            return false;
+        }
+        ParkingViolation p = (ParkingViolation) o;
+
+        return fine == p.fine && Objects.equals(timestamp, p.timestamp) &&
+                Objects.equals(description, p.description) && Objects.equals(vehicleId, p.vehicleId)
+                && Objects.equals(state, p.state) && Objects.equals(violationId, p.violationId) &&
+                Objects.equals(zipCode, p.zipCode);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(timestamp, fine, description, vehicleId, state, violationId, zipCode);
+    }
 
 }
