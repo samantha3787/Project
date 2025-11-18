480579b6de8725f70da3115a933f6e298dfce248
samanthakim <samanthakim3787@gmail.com>
2025-11-18 16:48:25 -0500

    population data class (data tier)

--- src/main/java/project/data/PopulationData.java
+++ src/main/java/project/data/PopulationData.java
@@ -1,4 +1,96 @@
 package project.data;
 
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class PopulationData {
+
+    private final Map<String, Integer> populationByZipCode = new HashMap<>();
+
+    public PopulationData(String filename) throws IOException {
+        fileLoader(filename);
+    }
+
+    private void fileLoader(String filename) throws IOException {
+        File file = new File(filename);
+
+        try(Scanner scanner = new Scanner(file)) {
+            while(scanner.hasNextLine()) {
+                String line = scanner.nextLine().trim();
+                if(line.isEmpty()) {
+                    continue;
+                }
+
+                String[] lineParts = line.split("\\s+");
+                if (lineParts.length < 2) {
+                    continue;
+                }
+
+                String pureZipCode = lineParts[0];
+                String purePopulationValue = lineParts[1];
+
+                String zipCode = processZipCode(pureZipCode);
+                if (zipCode == null){
+                    continue;
+                }
+
+                int population;
+                try {
+                    population = Integer.parseInt(purePopulationValue);
+                } catch (NumberFormatException e) {
+                    continue;
+                }
+
+                if(population<0) {
+                    continue;
+                }
+
+                populationByZipCode.put(zipCode, population);
+
+            }
+        } catch (FileNotFoundException e) {
+            throw new IOException("Could not open population file", e);
+        }
+    }
+
+    private String processZipCode(String zip) {
+        if(zip == null){
+            return null;
+        }
+        zip = zip.trim();
+        if(zip.isEmpty()) {
+            return null;
+        }
+        if(zip.length() >= 5) {
+            return zip.substring(0,5);
+        } else {
+            return zip;
+        }
+    }
+
+    public int getPopulation(String zipCode) {
+        if(zipCode == null) {
+            return 0;
+        }
+        Integer pop = populationByZipCode.get(zipCode);
+        return (pop==null) ? 0 : pop;
+    }
+
+    public Map<String, Integer> getAllPopulationData() {
+        return populationByZipCode;
+    }
+
+    public long getTotalPopulationSum() {
+        long sum = 0L;
+        for(int val : populationByZipCode.values()) {
+            sum += val;
+        }
+        return sum;
+    }
+
+
 }
